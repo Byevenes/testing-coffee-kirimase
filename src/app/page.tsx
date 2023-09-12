26243252cd65447654e1062f900377b062613304
Byevenes <zneel.yevenes@gmail.com>
2023-09-11 23:04:17 -0300

    add all components x shadcn && create auth login page x testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import SignIn from '@/components/auth/SignIn'
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <SignIn />
+    <>
+      <SignIn />
+      <Button variant={'destructive'}>destructive Button</Button>
+      <Button variant={'ghost'}>ghost Button</Button>
+      <Button variant={'link'}>link Button</Button>
+      <Button variant={'outline'}>outline Button</Button>
+      <Button variant={'secondary'}>secondary Button</Button>
+    </>
   )
 }
