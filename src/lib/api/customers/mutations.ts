212ae64eb94af252a45cc8f4cf12ddda1619a71c
Diego Garcia <dgarciavallejos@gmail.com>
2023-11-10 01:59:30 -0300

    Refactor customer mutations to handle birthday as a Date object.

--- src/lib/api/customers/mutations.ts
+++ src/lib/api/customers/mutations.ts
@@ -1,22 +1,22 @@
-import { db } from "@/lib/db/index";
 import { and, eq } from "drizzle-orm";
+
+import { getUserAuth } from "@/lib/auth/utils";
+import { db } from "@/lib/db/index";
 import { 
   CustomerId, 
+  customerIdSchema, 
+  customers,
+  insertCustomerSchema, 
   NewCustomerParams,
   UpdateCustomerParams, 
-  updateCustomerSchema,
-  insertCustomerSchema, 
-  customers,
-  customerIdSchema 
-} from "@/lib/db/schema/customers";
-import { getUserAuth } from "@/lib/auth/utils";
+  updateCustomerSchema } from "@/lib/db/schema/customers";
 
 export const createCustomer = async (customer: NewCustomerParams) => {  
   const { session } = await getUserAuth();
 
   try {
     const newCustomer = insertCustomerSchema.parse({ ...customer, userId: session?.user.id!, birthday: new Date(customer.birthday) });
-    const [c] =  await db.insert(customers).values(newCustomer).returning();
+    const [c] =  await db.insert(customers).values({ ...newCustomer, birthday: newCustomer.birthday.toDateString() }).returning();
     return { customer: c };
   } catch (err) {
     const message = (err as Error).message ?? "Error, please try again";
@@ -32,7 +32,7 @@ export const updateCustomer = async (id: CustomerId, customer: UpdateCustomerPar
     const newCustomer = updateCustomerSchema.parse({ ...customer, userId: session?.user.id! });
     const [c] =  await db
      .update(customers)
-     .set(newCustomer)
+     .set({ ...newCustomer, birthday: newCustomer.birthday.toDateString() })
      .where(and(eq(customers.id, customerId!), eq(customers.userId, session?.user.id!)))
      .returning();
     return { customer: c };
